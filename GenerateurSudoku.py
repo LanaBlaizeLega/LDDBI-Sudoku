98ddf80516856f1c04e3bc7bca65e3c2a18db0b6
Octologue <you@example.com>
2025-05-03 16:15:27 +0200

    gen

--- GenerateurSudoku.py
+++ GenerateurSudoku.py
@@ -1,94 +1,55 @@
 import numpy as np
 import random as r
 
-#faire une grille soluce vide avec 9 listes dedans.
-# fonction générer des nombres de 1 à 9 sans doublons
-# grillesoluce.append(fonction genere 9 nb)
-
-#fonction random first row(tablde0)
-
-#fonction qui génère des nombres
+listeTot = [1]*9 + [2]*9 + [3]*9 + [4]*9 + [5]*9 + [6]*9 + [7]*9 + [8]*9 + [9]*9
+r.shuffle(listeTot)
+tab = np.zeros((9,9), dtype=int)
 
 
-Tableau = np.zeros((9,9))
-print(Tableau)
+# Initialisation -----------------------------------------------------------------------------------
 
-L = [1,2,3,4,5,6,7,8,9]
+def GenererCarreAleatoire(x,y):
+    liste = [1,2,3,4,5,6,7,8,9]
+    r.shuffle(liste)
+    for i in range (x, x+3):
+        for j in range (y,y+3):
+            tab[i][j] = liste.pop()
 
+def GenererDiagonaleAleatoire():
+    GenererCarreAleatoire(0,0)
+    GenererCarreAleatoire(3,3)
+    GenererCarreAleatoire(6,6)
 
-def Reset():
-    for i in range(1,10):
-        L.append(i)
-  
-def GenEntier():
-    return L.pop(r.randint(0,len(L)-1))
+def SupprimerDeListeTot():
+    for lin in tab:
+        for elem in lin:
+            if elem != 0 and elem in listeTot:
+                listeTot.remove(elem)
 
-def LigneAleatoire(ligne):
-    for i in range(9):
-        Tableau[ligne][i] = GenEntier()
-    Reset()
-
-
-def CompareLigne(a,b):
-    for i in range (0,8):
-        if Tableau[a][i] == Tableau[b][i]:
-            # print("true")
-            return True
-    # print("false")
-    return False
-
-def CompareColonne(a,b):
-    for i in range (0,8):
-        if Tableau[i][a] == Tableau[i][b]:
-            # print("true")
-            return True
-    # print("false")
-    return False
+def Initialisation():
+    GenererDiagonaleAleatoire()
+    SupprimerDeListeTot()
 
 
-def ResetLAvecCarre(lin, col):
-    list_carre = []
-    for i in range(lin,lin+2):
-        for j in range(col,col+2):
-            list_carre.append(Tableau[i][j])
-    while 0 in list_carre:
-        list_carre.remove(0)
-    # return len(list_carre) != len(set(list_carre))
-    
-    for value in L:
-        while value in list_carre:
-            L.remove(value)
+# conditions sudoku --------------------------------------------------------------------------------
 
-def CheckLigne(x,y):
-    for i in range(0,8):
-        if i == x:
-            continue
-        else:
-            if Tableau[i][y] == Tableau[x][y]:
-                return True
-    return False
+def CheckLigne(tab, lin, col):
+    num = tab[lin][col]
+    if num == 0:
+        return True
+    for c in range(len(tab[lin])):
+        if c != col and tab[lin][c] == num:
+            return False
+    return True
 
-def CheckColonne(x,y):
-    for i in range(0,8):
-        if i == y:
-            continue
-        else:
-            if Tableau[x][i] == Tableau[x][y]:
-                return True
-    return False
-
-
-def InitialisationLignes():
-
-    LigneAleatoire(0)
-    LigneAleatoire(3)
-    LigneAleatoire(6)
-
-    while CompareLigne(0,3) :
-        LigneAleatoire(3) 
-
-    while CompareLigne(0,6) or CompareLigne(3,6):
-        LigneAleatoire(6)
+def CheckColonne(tab, lin, col):
+    num = tab[lin][col]
+    if num == 0:
+        return True
+    for l in range(len(tab[col])):
+        if l != lin and tab[l][col] == num:
+            return False
+    return True
 
 def IndiceCarre(i):
     if i >= 6:
@@ -98,29 +59,80 @@ def IndiceCarre(i):
     else:
         return 3
 
-def Completer():
-    for i in range(0, 9):
-        if i==0 or i==3 or i==6:
-            continue
-        else:   
-            for j in range(0,9):
-                carreX = IndiceCarre(i)
-                carreY = IndiceCarre(j)
+def CheckCarre(tab, lin, col):
+    carreX = IndiceCarre(lin)
+    carreY = IndiceCarre(col)
 
-                while  CheckLigne(i,j) and CheckColonne(i,j):
-                    ResetLAvecCarre(carreX,carreY)
-                    Tableau[i][j] = GenEntier()
-                   
+    listeCarre = []
+    for i in range (carreX, carreX+3):
+        for j in range (carreY,carreY+3):
+            if tab[i][j] != 0:
+                listeCarre.append(tab[i][j])
+    return len(listeCarre) == len(set(listeCarre))
+
+def CheckTotal(tab,lin,col):
+    return CheckLigne(tab, lin, col) and CheckColonne(tab, lin, col) and CheckCarre(tab, lin, col)
 
 
+# génération de la grille solution (aide de chat gpt) --------------------------------------------------------------------------
+
+def RemplirGrille():
+    return Backtrack(0, 0)
+
+def Backtrack(lin, col):
+    if lin == 9:
+        return True  
+
+    next_lin, next_col = (lin, col + 1) if col < 8 else (lin + 1, 0)
+
+    if tab[lin][col] != 0:
+        return Backtrack(next_lin, next_col)
+
+    for i in range(len(listeTot)):
+        val = listeTot[i]
+        tab[lin][col] = val
+        if CheckTotal(tab, lin, col):
+
+            temp = listeTot.pop(i)
+            if Backtrack(next_lin, next_col):
+                return True
             
+            listeTot.insert(i, temp)
+        tab[lin][col] = 0  
+
+    return False  
+
+def Creationtableau():
+    Initialisation()
+
+    if RemplirGrille():
+        print(tab)
+    else:
+        print("Échec de la génération.")
+  
+
+Creationtableau()
+
+
+# création grille à trous --------------------------------------------------------------------------
+
+def ViderCases(num):
+
+    indices = [(i, j) for i in range(9) for j in range(9)]
+    r.shuffle(indices)
+    
+    for k in range(num):
+        i, j = indices[k]
+        tab[i][j] = 0
+                    
+ViderCases(50)
+
+print(tab)
+
+
+# algorithme pour vérifier qu'il n'y ait qu'une seule solution -------------------------------------
 
 
 
-
-InitialisationLignes()
-Completer()
-print(L)
-print(Tableau)
 
 
